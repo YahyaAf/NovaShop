3cba00c3283df1ea291305ecff9f619120c79917
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-11-25 21:05:29 +0100

    ajouter structure de project

---
+++ src/main/java/org/project/novashop/NovaShopApplication.java
@@ -0,0 +1,13 @@
+package org.project.novashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/novashop/NovaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.novashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
